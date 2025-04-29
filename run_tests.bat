95be5b5b741577aad1dae89582de0934a7d9979b
yjmantilla <yjmantilla@gmail.com>
2025-04-28 22:03:47 -0400

    Add distribution test to the run_tests.bat script

--- run_tests.bat
+++ run_tests.bat
@@ -1,4 +1,5 @@
 python tri_hybride_test.py
 python foresterie_test.py
+python distribution_test.py
 g++ -o .\min_cost_recharge.exe .\min_cost_recharge.cpp .\MinCostRechargeCalculator.cpp
 .\min_cost_recharge.exe
