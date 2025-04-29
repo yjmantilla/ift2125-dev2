5d71ac736f12cba58042af41c0e3d514a11d7b3e
yjmantilla <yjmantilla@gmail.com>
2025-04-28 22:05:31 -0400

    Add C++ test execution to run_tests.bat and ensure distribution test is included

---
+++ run_cpp_tests.sh
@@ -0,0 +1,3 @@
+# run in git bash
+g++ -o ./min_cost_recharge.exe ./min_cost_recharge.cpp ./MinCostRechargeCalculator.cpp
+./min_cost_recharge.exe
--- run_tests.bat
+++ run_tests.bat
@@ -1,5 +1,3 @@
 python tri_hybride_test.py
 python foresterie_test.py
 python distribution_test.py
-g++ -o .\min_cost_recharge.exe .\min_cost_recharge.cpp .\MinCostRechargeCalculator.cpp
-.\min_cost_recharge.exe
